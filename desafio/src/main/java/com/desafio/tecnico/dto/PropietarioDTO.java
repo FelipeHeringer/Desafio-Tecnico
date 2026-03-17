aa39be240e3d46b5587236dc953a107e1e2cead7
FelipeHeringer <heringerfelipe14@gmail.com>
2026-03-17 19:08:29 -0300

    feat: adicionar Classes DTO para gerenciamento da View

---
+++ desafio/src/main/java/com/desafio/tecnico/dto/PropietarioDTO.java
@@ -0,0 +1,45 @@
+package com.desafio.tecnico.dto;
+
+public class PropietarioDTO {
+
+    private String nome;
+    private String cpf;
+    private String email;
+    private String telefone;
+    private EnderecoDTO endereco;
+    
+
+    public PropietarioDTO() {
+        this.endereco = new EnderecoDTO();
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    public EnderecoDTO getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(EnderecoDTO endereco) {
+        this.endereco = endereco;
+    }
+}
