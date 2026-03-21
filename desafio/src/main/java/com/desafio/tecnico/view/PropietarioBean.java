3e0d02a21bf8fdd237f9d5c26186168367d976f8
FelipeHeringer <heringerfelipe14@gmail.com>
2026-03-21 12:19:27 -0300

    feat: adicionar Bean para Gerenciamento do Propietario

---
+++ desafio/src/main/java/com/desafio/tecnico/view/PropietarioBean.java
@@ -0,0 +1,50 @@
+package com.desafio.tecnico.view;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.desafio.tecnico.dto.PropietarioDTO;
+import com.desafio.tecnico.entity.Propietario;
+import com.desafio.tecnico.service.PropietarioService;
+
+@Named("propietarioBean")
+@SessionScoped
+public class PropietarioBean implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Inject
+    private PropietarioService propietarioService;
+    
+    private PropietarioDTO propietario = new PropietarioDTO();
+
+    public PropietarioBean() {
+        
+    }
+
+    public void salvar() {
+        propietarioService.salvar(propietario);
+
+    }
+
+    public String entrar() {
+        Propietario propietarioLogado = propietarioService.entrar(propietario);
+
+        if (propietarioLogado != null) {
+            return "GestaoCasas.xhtml?faces-redirect=true";
+        }
+
+        return "Login.xhtml";
+    }
+
+    public PropietarioDTO getPropietario() {
+        return propietario;
+    }
+
+    public void setPropietario(PropietarioDTO propietario) {
+        this.propietario = propietario;
+    }
+}
